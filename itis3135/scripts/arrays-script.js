216f5a5211851c0b3a19037f07a85fc97e19bc92
mnguyen56 <mnguye56@uncc.edu>
2021-10-18 23:03:17 -0400

    project design

---
+++ itis3135/scripts/arrays-script.js
@@ -0,0 +1,16 @@
+var person = ["Feng Min", "Meg Thomas", "Leon Kennedy", "Bill Overbeck", "Yui Kimura", "Claudette Morel", "Nancy Wheeler"];
+var salaries = [26000, 6600, 18800, 12500, 10300, 7500, 7200];
+
+
+
+function addSalary() {
+
+}
+
+function displayResults() {
+
+}
+
+function displaySalary() {
+
+}
